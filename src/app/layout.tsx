490fd65616ecaa4ae2ebae5c7d36d01fb62accb5
Andrea <battimielloandrea@gmail.com>
2026-05-17 18:51:42 +0200

    changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans  } from "next/font/google";
 import "./globals.css";
 import { AppShell } from "@/components/layout/AppShell";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -24,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="it">
-      <body >
+    <html lang="it" className={jakarta.variable}>
+      <body className={`antialiased`}>
         <AppShell>{children}</AppShell>
       </body>
     </html>
